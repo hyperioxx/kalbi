0bc665bcd9288e1bcca16e7e1eec7a11d6697d14
aaron <aaronparfitt123@gmail.com>
2020-10-10 19:38:52 +0100

    fix stupid mistake

--- sip/transaction/client.go
+++ sip/transaction/client.go
@@ -101,7 +101,6 @@ func (ct *ClientTransaction) SetListeningPoint(lp transport.ListeningPoint) {
 	ct.ListeningPoint = lp
 }
 
-func (ct *ClientTransaction) 
 
 func (ct *ClientTransaction) GetBranchId() string {
 	return ct.BranchID
