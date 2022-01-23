31c8a94fc757e579212e3cfb0d4a113fe037ee1f
Peter Donald <peterdonald3@outlook.com>
2022-01-23 13:51:08 +0000

    updated GenerateDialogId() to include rand.Seed function
    added tests for GenerateDialogId()

--- sip/dialog/utils_test.go
+++ sip/dialog/utils_test.go
@@ -1,17 +1,27 @@
 package dialog
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestGenerateDialogId(t *testing.T) {
 	tests := []struct {
 		name string
-		want int32
+		want bool
 	}{
-		// TODO: Add test cases.
+		{"test 1 Generate DialogID - returns int32", true},
+		{"test 2 Generate DialogID - returns int32", true},
+		{"test 3 Generate DialogID - returns int32", true},
+		{"test 4 Generate DialogID - returns int32", true},
+		{"test 5 Generate DialogID - returns int32", true},
+		{"test 6 Generate DialogID - returns int32", true},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GenerateDialogId(); got != tt.want {
+			got := GenerateDialogId()
+			if reflect.TypeOf(got).Kind() != reflect.Int32 {
 				t.Errorf("GenerateDialogId() = %v, want %v", got, tt.want)
 			}
 		})
