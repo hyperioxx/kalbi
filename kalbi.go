c7d4b5f98e88b7127637d45a7b5608c7c26abfde
hyperioxx <aaronparfitt123@gmail.com>
2022-07-27 11:45:28 +0100

    comments on exported functions

--- kalbi.go
+++ kalbi.go
@@ -53,58 +53,72 @@ func (ed *SipStack) SetSipListener(listener message.SipListener) {
 	ed.sipListener = listener
 }
 
+//INVITE used to attach a callback function when an INVITE is received 
 func (ed *SipStack) INVITE(handler func(message.SipEventObject)) {
 	ed.funcMap["INVITE"] = handler
 }
 
+//ACK used to attach a callback function when an ACK is received 
 func (ed *SipStack) ACK(handler func(message.SipEventObject)) {
 	ed.funcMap["ACK"] = handler
 }
 
+//BYE used to attach a callback function when an BYE is received 
 func (ed *SipStack) BYE(handler func(message.SipEventObject)) {
 	ed.funcMap["BYE"] = handler
 }
 
+//CANCEL used to attach a callback function when an CANCEL is received 
 func (ed *SipStack) CANCEL(handler func(message.SipEventObject)) {
 	ed.funcMap["CANCEL"] = handler
 }
 
+//REGISTER used to attach a callback function when an REGISTER is received
 func (ed *SipStack) REGISTER(handler func(message.SipEventObject)) {
 	ed.funcMap["REGISTER"] = handler
 }
 
+//INFO used to attach a callback function when an INFO is received
 func (ed *SipStack) INFO(handler func(message.SipEventObject)) {
 	ed.funcMap["INFO"] = handler
 }
 
+//OPTIONS used to attach a callback function when an OPTIONS is received
 func (ed *SipStack) OPTIONS(handler func(message.SipEventObject)) {
 	ed.funcMap["OPTIONS"] = handler
 }
 
+//PRACK used to attach a callback function when an PRACK is received
 func (ed *SipStack) PRACK(handler func(message.SipEventObject)) {
 	ed.funcMap["PRACK"] = handler
 }
 
+//SUBSCRIBE used to attach a callback function when an SUBSCRIBE is received
 func (ed *SipStack) SUBSCRIBE(handler func(message.SipEventObject)) {
 	ed.funcMap["SUBSCRIBE"] = handler
 }
 
+//NOTIFY used to attach a callback function when an NOTIFY is received
 func (ed *SipStack) NOTIFY(handler func(message.SipEventObject)) {
 	ed.funcMap["NOTIFY"] = handler
 }
 
+//PUBLISH used to attach a callback function when an PUBLISH is received
 func (ed *SipStack) PUBLISH(handler func(message.SipEventObject)) {
 	ed.funcMap["PUBLISH"] = handler
 }
 
+//REFER used to attach a callback function when an REFER is received
 func (ed *SipStack) REFER(handler func(message.SipEventObject)) {
 	ed.funcMap["REFER"] = handler
 }
 
+//MESSAGE used to attach a callback function when an MESSAGE is received
 func (ed *SipStack) MESSAGE(handler func(message.SipEventObject)) {
 	ed.funcMap["MESSAGE"] = handler
 }
 
+//UPDATE used to attach a callback function when an UPDATE is received
 func (ed *SipStack) UPDATE(handler func(message.SipEventObject)) {
 	ed.funcMap["UPDATE"] = handler
 }
